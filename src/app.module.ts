bc47bd745426eefe8563be9f58123f13c1e6d16f
RodCarvalhoas <sutil_rodrigo@hotmail.com>
2024-10-15 22:21:31 -0300

    adiciona logs a nivel global

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,23 @@ import { Module } from '@nestjs/common';
 import { ScopesModule } from './scopes/scopes.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { dataSourceOptions } from './infra/db/data-source';
+import { LoggerModule } from 'nestjs-pino';
 
 @Module({
   imports: [
     ScopesModule,
     TypeOrmModule.forRoot(dataSourceOptions),
+    LoggerModule.forRoot({
+      pinoHttp: {
+        quietReqLogger: true,
+        mixin: () => ({ env: process.env.NODE_ENV ?? 'UNKNOWN' }),
+        formatters: {
+          level(label, _number) {
+            return { level: label };
+          }
+        }
+      },
+    }),
   ],
   controllers: [],
   providers: [],
